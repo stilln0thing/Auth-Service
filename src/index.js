9cc9d0800f06a5b3e66b629d66a022ec5b5ae9e4
MachineMaven <mathcrayon12@gmail.com>
2024-07-27 18:51:02 +0530

    Added signup feature

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const bodyParser  = require('body-parser');
 
-const { PORT } = require('./config/serverConfig')
+const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+
+// const { User } = require('./models/index');
+// const bcrypt = require('bcrypt');
+
 const app = express();
 
 const prepareAndStartServer = () => {
 
-   app.listen(3000, () =>{
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use('/api', apiRoutes);
+
+   app.listen(3000, async () =>{
       console.log(`Server Started on Port: ${PORT}`);
+    //   const incomingpassword = 'herono1';
+    //   const user = await User.findByPk(4);
+    //   const response = bcrypt.compareSync(incomingpassword, user.password);
+    //   console.log(response);
    });
 }
 
